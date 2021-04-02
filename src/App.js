ab634fc553103ce0ac8fd377bbda561a103945b4
Rebecca Ross <b.laurenross@gmail.com>
2021-04-02 02:32:09 +0100

    created Nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { component } from "react";
 import "./App.css";
 import Vehicle from "./Vehicle/Vehicle";
+import Nav from "./Nav/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>Trac API</h1>
       <Vehicle />
     </div>
---
+++ src/Nav/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <div className="navbar">
+      <img
+        className="logo"
+        src={
+          "https://storage.googleapis.com/craft-images/company-logos/1519-f_C33FEtdNN48vCjxxqDsjW2pcME5PB7um9z63RIzuU.png"
+        }
+      />
+    </div>
+  );
+};
+
+export default Nav;
