f82f1b867ae83155de9692aabad0c5298daf3112
Rebecca Ross <b.laurenross@gmail.com>
2021-04-02 03:50:56 +0100

    styled dashboard

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -4,10 +4,26 @@ import "./Dashboard.css";
 const Dashboard = () => {
   return (
     <div className="panels">
-      <div className="panel-menu"></div>
-      <div className="panel-allocation"></div>
-      <div className="panel-balance"></div>
-      <div className="panel-positions"></div>
+      <div className="panel-menu">
+        <li>KPI Dashboard</li>
+        <li>Task Map</li>
+        <li>Task List</li>
+        <li>Task Creation</li>
+        <li>Rule Setting</li>
+        <li>Vehicle Information</li>
+        <li className="vehicle-list">Vehicle List</li>
+      </div>
+      <div className="panel-map">
+        <div className="map-box">
+          <h5>Vehicle Map</h5>
+        </div>
+      </div>
+      <div className="panel-search">
+        <div className="search-box">
+          <h5>Vehicle Search</h5>
+        </div>
+      </div>
+      <div className="panel-vehicles"></div>
     </div>
   );
 };
