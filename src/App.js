f05e8e3e16c9c81d5087c73ee3bdd78013998e44
Rebecca Ross <b.laurenross@gmail.com>
2021-04-02 01:52:47 +0100

    created Vehicle component and called api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React, { component } from "react";
 import "./App.css";
+import Vehicle from "./Vehicle/Vehicle";
 
 function App() {
   return (
     <div className="App">
       <h1>Trac API</h1>
+      <Vehicle />
     </div>
   );
 }
---
+++ src/Vehicle/Vehicle.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+//http://console-api.tracmobility.com/test/vehicles?page=0&size=10
+
+const Vehicle = () => {
+  const [api, setApi] = useState([]);
+
+  useEffect(() => {
+    const getVehicles = async () => {
+      const response = await fetch(
+        "http://console-api.tracmobility.com/test/vehicles?page=0&size=10"
+      );
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+      setApi(jsonResponse);
+    };
+    getVehicles();
+  }, []);
+
+  return (
+    <div>
+      <h1>Vehicles</h1>
+      <ul>
+        {/* {jsonResponse.map((response) => {
+          return <li>{response.content}</li>;
+        })} */}
+      </ul>
+    </div>
+  );
+};
+
+//   const Comment = ({ jsonResponse }) => {
+//     return (
+//       <ul>
+//         {jsonResponse.map((data) => {
+//           return (
+//             <li>
+//               <p{data.content}</p>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     );
+//   };
+// };
+
+export default Vehicle;
